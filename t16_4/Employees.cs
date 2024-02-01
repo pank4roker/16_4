fdab4e402aa488f210e924c7b438522d08ec419f
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-02-01 11:17:24 +0200

    Add files via upload

---
+++ t16_4/Employees.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace t16_4
+{
+    /// <summary>
+    /// Класс сотрудников
+    /// </summary>
+    class Employees
+    {
+        /// <summary>
+        /// поле описывающее табельный номер
+        /// </summary>
+        private int number;
+
+        public int Number { get => (number > 0) ? number:0; set => number = value; }
+        /// <summary>
+        /// поле описывающее имя
+        /// </summary>
+        private string name;
+        public string Name { get => (name != " ") ? name : "Unknown"; set => name = value; }
+        /// <summary>
+        /// поле описывающее фамилию
+        /// </summary>
+        private string surname;
+        public string Surname { get => (surname != " ") ? surname : "Unknown"; set => surname = value; }
+        /// <summary>
+        /// поле описывающее отчество
+        /// </summary>
+        private string otchestvo;
+        public string Otchestvo { get => (otchestvo != " ") ? otchestvo:"Unknown"; set => otchestvo = value; }
+        /// <summary>
+        /// поле описывающее дату рождения
+        /// </summary>
+        private DateTime dateofbirth;
+        public DateTime Dateofbirth { get => dateofbirth; set => dateofbirth = value; }
+        /// <summary>
+        /// поле описывающее оклад
+        /// </summary>
+        private double salary;
+        public double Salary { get => (salary >= 0) ?  salary:0; set => salary = value; }
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Employees() { }
+        /// <summary>
+        /// Конструктор с параметров
+        /// </summary>
+        public Employees(int number, string name, string surname, string otchestvo, DateTime dateofbirth, double salary)
+        {
+            this.number = number;
+            this.name = name;
+            this.surname = surname;
+            this.otchestvo = otchestvo;
+            this.dateofbirth = dateofbirth;
+            this.salary = salary;  
+        }
+        /// <summary>
+        /// Метод вывода
+        /// </summary>
+        public void OutPut()
+        {
+            WriteLine($"Табельный номер:{Number}\nИмя:{Name}\nФамилия:{Surname}\nОтчество:{Otchestvo}\nДата рождения:{Dateofbirth}\nОклад:{Salary}");
+        }
+        public void Out()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ t16_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace t16_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Введите количество сотрудников:");
+            int n = Convert.ToInt32(ReadLine());
+            Employees[] employees = new Employees[n];
+            for(int i =0;i<employees.Length; i++)
+            {
+                Write("Введите табельтный номер: ");
+                int number = Convert.ToInt32(ReadLine());
+                Write("Введите имя: ");
+                string name = ReadLine();
+                Write("Введите фамилию: ");
+                string surname = ReadLine();
+                Write("Введите отчество: ");
+                string otchestvo = ReadLine();
+                Write("Введите дату рождения: ");
+                DateTime dayofbirth = DateTime.Parse(ReadLine());
+                Write("Введите оклад: ");
+                double salary = Convert.ToDouble(ReadLine());
+                employees[i] = new Employees(number,name,surname,otchestvo,dayofbirth,salary);
+                WriteLine();
+                
+            }
+            foreach(var employee in employees)
+            {
+                employee.OutPut();
+                WriteLine();
+            }
+            ReadKey();
+        }
+    }
+}
